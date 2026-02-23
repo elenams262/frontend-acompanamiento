5f8793093fc78d1031e3df7c0193615799914a84
Elena Martínez <elenams2000@hotmail.com>
2026-02-23 13:32:55 +0100

    responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,4 +8,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+}
